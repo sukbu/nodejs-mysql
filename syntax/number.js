47c21d4d1f452e5ef3287917f022147437a154bd
sukbu <tjrqja12345@gmail.com>
2019-10-19 19:08:28 +0900

    Initial commit

---
+++ syntax/number.js
@@ -0,0 +1,13 @@
+//  Javascript data types:
+//  Number, String, Boolean, Array, Object, ...
+console.log(1+1);
+console.log('1'+1);
+console.log(1+'1');
+console.log(4-'1');
+console.log(4-1);
+console.log(2*2);
+console.log(2*'2');
+console.log(10/2);
+console.log(Math.acos(-1));
+console.log(Math.atan(1)*4);
+console.log(Math.tan(0.5));
